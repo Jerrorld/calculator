6f4eeefe1be6d32f133bb872134cdab5f38b52bb
Jerrorld <jerrorldk@gmail.com>
2022-06-14 20:52:59 -0700

    last commit is false, unexpected error

--- assets/script.js
+++ assets/script.js
@@ -83,15 +83,3 @@ clear.addEventListener("click", function() {
     input.innerHTML = "";
 })
 
-
-/* function display(val) {
-    document.getElementById("textval").value+=val
-};
-function evaluate() {
-    let x = document.getElementById("textval").value 
-    let y = eval(x)
-    document.getElementById("textval").value = y
-};
-function clr() {
-    document.getElementById("textval").value = ""
-}
